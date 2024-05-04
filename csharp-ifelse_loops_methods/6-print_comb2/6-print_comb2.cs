b12817b8c47324743322d3d8e432b40cbb75f877
Collinb190 <collinb190@gmail.com>
2024-05-04 09:54:32 -0500

    task 6

---
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int k = 1; k < 10; k++)
+                {
+                    if (i < k && i != 8 && k != 9)
+                    {
+                        Console.Write($"{i}{k}, ");
+                    }
+                    else if (i == 8 && k == 9)
+                    {
+                        Console.WriteLine($"{i}{k}");
+                    }
+                }
+            }
+        }
+    }
+}
