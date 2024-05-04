66435d15be97d955adf51c3ea14fcee5f69d4904
Collinb190 <collinb190@gmail.com>
2024-05-04 11:38:58 -0500

    task 10

---
+++ csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _10_print_line
+{
+    class Line
+    {
+        public static void PrintLine(int length)
+        {
+            if (length > 0)
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    Console.Write("_");
+                }
+            }
+            Console.WriteLine("");
+        }
+    }
+}
