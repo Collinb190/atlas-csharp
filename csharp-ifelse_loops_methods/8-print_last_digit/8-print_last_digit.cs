d34110262b00922f209f1d1d31e1c70fc86111c1
Collinb190 <collinb190@gmail.com>
2024-05-04 10:47:29 -0500

    task 8

---
+++ csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _8_print_last_digit
+{
+    class Number
+    {
+        public static int PrintLastDigit(int number)
+        {
+            number = Math.Abs(number);
+            number %= 10;
+            Console.Write(number);
+            return number;
+        }
+    }
+}
