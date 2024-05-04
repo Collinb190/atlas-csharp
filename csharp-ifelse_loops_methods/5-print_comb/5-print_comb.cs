f7e716dccfce3782ec2c22e7c9377eb84eb3ce8a
Collinb190 <collinb190@gmail.com>
2024-05-04 00:49:14 -0500

    task 5

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                if (i < 99)
+                {
+                    Console.Write(i.ToString("D2") + ", ");
+                }
+                else
+                {
+                    Console.Write(i + "\n");
+                }
+            }
+        }
+    }
+}
