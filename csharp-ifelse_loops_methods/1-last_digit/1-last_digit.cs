3e8147a59bd0334afd0c678ad37994e4aeae2f8d
Collinb190 <collinb190@gmail.com>
2024-05-03 23:50:21 -0500

    task 1

---
+++ csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10000, 10000);
+	    int remainder = number % 10;
+
+        if (remainder == 0)
+        {
+            Console.WriteLine($"The last digit of {number} is {remainder} and is 0");
+        }
+        else if (remainder > 5)
+        {
+            Console.WriteLine($"The last digit of {number} is {remainder} and is greater than 5");
+        }
+        else
+        {
+            Console.WriteLine($"The last digit of {number} is {remainder} and is less than 6 and not 0");
+        }
+    }
+}
