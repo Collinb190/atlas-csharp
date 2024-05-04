62e9e9872998e0f72f6f6375b0b1c4674f7bcef6
Collinb190 <collinb190@gmail.com>
2024-05-04 00:01:49 -0500

    task 2

---
+++ csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 97; i <= 122; i++)
+            {
+                char letter = (char)i;
+                Console.Write(letter);
+            }
+        }
+    }
+}
