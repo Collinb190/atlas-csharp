2c61bbe772ac931dc1e852911b1a5045228d8451
Collinb190 <collinb190@gmail.com>
2024-05-05 11:01:49 -0500

    checker sucks

--- csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
+++ csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -7,8 +7,7 @@ class Array
         int x = array[index];
         if (index < 0 || index > array.Length - 1)
         {
-            Console.WriteLine("Index out of range");
-            return -1;
+            return;
         }
         return x;
     }
