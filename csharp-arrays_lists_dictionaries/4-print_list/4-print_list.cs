5a10d95cd5e33b55df23a42bc83e70f9acf3848a
Collinb190 <collinb190@gmail.com>
2024-05-05 18:37:24 -0500

    task 4

---
+++ csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        List<int> numbers = new List<int>();
+        for (int i = 0; i < size; i++)
+        {
+            numbers.Add(i);
+        }
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            Console.Write("{0}{1}", numbers[i], i == numbers.Count - 1 ? "":" ");
+        }
+        Console.WriteLine();
+        return numbers;
+    }
+}
