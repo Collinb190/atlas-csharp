8d286fc870b88ebc7e0dafed568b5d094fe0eae0
Collinb190 <collinb190@gmail.com>
2024-05-05 10:53:26 -0500

    task 1

---
+++ csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Array
+{
+    public static int elementAt(int[] array, int index)
+    {
+        int x = array[index];
+        if (index < 0 || index > array.Length - 1)
+        {
+            Console.WriteLine("Index out of range");
+            return -1;
+        }
+        return x;
+    }
+}
