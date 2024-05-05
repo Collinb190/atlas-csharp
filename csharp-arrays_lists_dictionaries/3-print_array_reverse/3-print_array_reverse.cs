2c737e604c59db8a90f996e722a84b01ef6e6eb5
Collinb190 <collinb190@gmail.com>
2024-05-05 12:21:53 -0500

    task 3

---
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Array
+{
+    public static void Reverse(int[] array)
+    {
+        if (array == null)
+        {
+            Console.WriteLine();
+            return;
+        }
+        else if (array.Length == 0)
+        {
+            Console.WriteLine();
+            return;
+        }
+        for (int i = array.Length - 1; i >= 0; i--)
+        {
+            Console.Write("{0}{1}", array[i], i != 0 ? " ": "");
+        }
+        Console.WriteLine();
+    }
+}
