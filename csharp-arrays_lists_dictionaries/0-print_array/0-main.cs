9cb8f1934094bf519f54a11fd7246e221ab14791
Collinb190 <collinb190@gmail.com>
2024-05-05 10:18:49 -0500

    0

--- csharp-arrays_lists_dictionaries/0-print_array/0-main.cs
+++ csharp-arrays_lists_dictionaries/0-print_array/0-main.cs
@@ -6,15 +6,15 @@ class Program
     {
         int[] newArray;
 
-        newArray = _0_print_array.Array.CreatePrint(10);
+        newArray = Array.CreatePrint(10);
         Console.WriteLine("Array Length: " + newArray.Length);
         Console.WriteLine("----------------");
-        newArray = _0_print_array.Array.CreatePrint(16);
+        newArray = Array.CreatePrint(16);
         Console.WriteLine("Array Length: " + newArray.Length);
         Console.WriteLine("----------------");
-        newArray = _0_print_array.Array.CreatePrint(0);
+        newArray = Array.CreatePrint(0);
         Console.WriteLine("Array Length: " + newArray.Length);
         Console.WriteLine("----------------");
-        newArray = _0_print_array.Array.CreatePrint(-10);
+        newArray = Array.CreatePrint(-10);
     }
 }
