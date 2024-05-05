49e6dc3f82701eac2735f41e32283efc8c4d7c4d
Collinb190 <collinb190@gmail.com>
2024-05-05 11:08:07 -0500

    crazy

--- csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
+++ csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -1,14 +1,14 @@
-﻿using System;
+﻿﻿using System;
 
 class Array
 {
     public static int elementAt(int[] array, int index)
     {
-        int x = array[index];
         if (index < 0 || index > array.Length - 1)
-        {
-            return;
+        { 
+            Console.WriteLine("Index out of range");
+            return -1;
         }
-        return x;
+        return array[index];
     }
 }
