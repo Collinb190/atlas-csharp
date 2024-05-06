00f937d4724f02504ca5269da124bb161cbf5b51
Collinb190 <collinb190@gmail.com>
2024-05-05 23:06:36 -0500

    task 6

---
+++ csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+   public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        List<bool> multiple = new List<bool>();
+        foreach (int num in myList)
+        {
+            if  (num % 2 == 0)
+            {
+                multiple.Add(true);
+            }
+            else
+            {
+                multiple.Add(false);
+            }
+        }
+        return multiple;
+    }
+}
