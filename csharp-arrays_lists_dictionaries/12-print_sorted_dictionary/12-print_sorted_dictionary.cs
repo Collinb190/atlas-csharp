d5cdf6820b016fe609c5082f51d08345b4add374
Collinb190 <collinb190@gmail.com>
2024-05-06 08:17:14 -0500

    task 12

---
+++ csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary
+{
+   public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        List<string> sortedKeys = myDict.Keys.ToList();
+        sortedKeys.Sort();
+        foreach (string key in sortedKeys)
+        {
+            string value = myDict[key];
+            Console.WriteLine("{0}: {1}", key, value);
+        }
+    }
+}
