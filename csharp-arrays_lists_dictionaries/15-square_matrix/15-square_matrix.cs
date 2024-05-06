b0a214f86e2201a9b8b30bdedb8d2db90fb1bd44
Collinb190 <collinb190@gmail.com>
2024-05-06 11:44:21 -0500

    task 15

---
+++ csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Matrix
+{
+    public static int[,] Square(int[,] myMatrix)
+    {
+        int numRows = myMatrix.GetLength(0);
+        int numColums = myMatrix.GetLength(1);
+
+        for (int i = 0; i < numRows; i++)
+        {
+            for (int k = 0; k < numColums; k++)
+            myMatrix[i, k] *= myMatrix[i, k];
+        }
+        return myMatrix;
+    }
+}
