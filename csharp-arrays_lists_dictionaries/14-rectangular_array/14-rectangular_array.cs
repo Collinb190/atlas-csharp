f9a3e99a9aac4612f535cd62ff6da87e924cfe18
Collinb190 <collinb190@gmail.com>
2024-05-06 11:30:33 -0500

    task 14

---
+++ csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _14_rectangular_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] square = new int[5, 5];
+            square[2, 2] = 1;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int k = 0; k < 5; k++)
+                {
+                    Console.Write(square[i, k]);
+                    if (k < 4)
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
