e2a2000773e1af43d1c41bb195dd8c801ae8632f
Collinb190 <collinb190@gmail.com>
2024-05-06 18:20:35 -0500

    task 2

--- csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
+++ csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -1,10 +1,27 @@
-﻿uusing System;
+﻿using System;
 using System.Collections.Generic;
 
 class List
 {
   public static List<int> DifferentElements(List<int> list1, List<int> list2)
     {
-        
+        List<int> sorted = new List<int>();
+        foreach (int num in list1)
+        {
+            if (!list2.Contains(num))
+            {
+                sorted.Add(num);
+            }
+        }
+
+        foreach (int num in list2)
+        {
+            if (!list1.Contains(num))
+            {
+                sorted.Add(num);
+            }
+        }
+        sorted.Sort();
+        return sorted;
     }
 }
