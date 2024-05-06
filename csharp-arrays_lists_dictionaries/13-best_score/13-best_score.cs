09ca6be3bf2824a4e44e4c5e39d6b62dc78b1ba8
Collinb190 <collinb190@gmail.com>
2024-05-06 08:49:45 -0500

    task 13

---
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary
+{
+   public static string BestScore(Dictionary<string, int> myList)
+    {
+        string bestKey = "";
+        int maxValue = int.MinValue;
+
+        if (myList.Count == 0)
+        {
+            return "None";
+        }
+        foreach (KeyValuePair<string, int> pair in myList)
+        {
+            if (pair.Value > maxValue)
+            {
+                maxValue = pair.Value;
+                bestKey = pair.Key;
+            }
+        }
+        return bestKey;
+    }
+}
