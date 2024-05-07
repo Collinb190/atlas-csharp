0712f319145626fb014ccc31c59d6c21db82d07a
Collinb190 <collinb190@gmail.com>
2024-05-07 12:11:18 -0500

    task 13

---
+++ csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+   public static void Delete(LinkedList<int> myLList, int index)
+    {
+        LinkedListNode<int> result = myLList.First;
+        int counter = 0;
+        while(result != null)
+        {
+            if (counter == index)
+                myLList.Remove(result);
+            counter += 1;
+            result = result.Next;
+        }
+    }
+}
