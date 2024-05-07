9908b19db9bbd680143ef03c717af9870d275811
Collinb190 <collinb190@gmail.com>
2024-05-07 09:59:51 -0500

    task 8

---
+++ csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+   public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        int index = 0;
+
+        if (myLList.Contains(value))
+        {
+            foreach (int i in myLList)
+            {
+                if (i == value)
+                    return(index);
+                index += 1;
+            }
+        }
+        else
+        {
+            return -1;
+        }
+        return -1;
+    }
+}
