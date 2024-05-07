41298483d8bbd19d1aa597a22e56b4ee0f86db68
Collinb190 <collinb190@gmail.com>
2024-05-07 12:02:29 -0500

    task 12

---
+++ csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+   public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> result = myLList.First;
+        while(result != null)
+        {
+            if (result.Value < n)
+                result = result.Next;
+            else
+            {
+                myLList.AddBefore(result, n);
+                return myLList.First;
+            }
+        }
+        myLList.AddLast(n);
+        return myLList.First;
+    }
+}
