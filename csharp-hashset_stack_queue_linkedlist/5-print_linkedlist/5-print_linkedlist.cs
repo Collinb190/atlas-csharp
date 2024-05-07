cfb0a5db6d88353be6dcfc2b33e69befdde14b8c
Collinb190 <collinb190@gmail.com>
2024-05-07 09:19:07 -0500

    task 5

---
+++ csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+  public static LinkedList<int> CreatePrint(int size)
+    {
+        List<int> template = new List<int>();
+
+        if (size < 0)
+        {
+           LinkedList<int> empty = new LinkedList<int>();
+           return empty;
+        }
+
+        for (int i = 0; i < size; i++)
+        {
+            template.Add(i);
+            Console.WriteLine(i);
+        }
+        LinkedList<int> result = new LinkedList<int>(template);
+        return result;
+    }
+}
