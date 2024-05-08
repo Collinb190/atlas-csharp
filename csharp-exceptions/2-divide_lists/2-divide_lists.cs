4ba9a62002e0a857a00d5687c99d23cb14a4aa7d
Collinb190 <collinb190@gmail.com>
2024-05-07 23:40:24 -0500

    task 2

--- csharp-exceptions/2-divide_lists/2-divide_lists.cs
+++ csharp-exceptions/2-divide_lists/2-divide_lists.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace _2_divide_lists
+class List
 {
-    class Program
+   public static List<int> Divide(List<int> list1, List<int> list2, int listLength)
     {
-        static void Main(string[] args)
+        List<int> result = new List<int>();
+        int i = 0;
+        while(i < listLength)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                result.Add(list1[i]/list2[i]);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Cannot divide by zero");
+                result.Add(0);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Out of range");
+            }
+            i++;
         }
+        return result;
     }
 }
