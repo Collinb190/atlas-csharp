b791d86bc5722e91d22a69df49c3b5341438f5a5
Collinb190 <collinb190@gmail.com>
2024-05-08 15:11:52 -0500

    task 2

---
+++ csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// An empty public class
+    /// </summary>
+    class Zombie
+    {
+        public int health;
+
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+        }
+    }
+}
