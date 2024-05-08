5362d48d86a75eb45ecb81977c53842097c9705f
Collinb190 <collinb190@gmail.com>
2024-05-08 15:17:03 -0500

    task 2 fix

--- csharp-classes/2-enemy/2-enemy.cs
+++ csharp-classes/2-enemy/2-enemy.cs
@@ -20,6 +20,7 @@ namespace Enemies
             {
                 throw new ArgumentException("Health must be greater than or equal to 0");
             }
+            health = value;
         }
     }
 }
