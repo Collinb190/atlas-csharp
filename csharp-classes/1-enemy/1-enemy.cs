7a77ccfa7c5c6f228a42a9c22eabfcbb6cd721ca
Collinb190 <collinb190@gmail.com>
2024-05-08 14:56:36 -0500

    task 1

---
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// An empty public class
+    /// </summary>
+    class Zombie
+    {
+        public int health;
+
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
