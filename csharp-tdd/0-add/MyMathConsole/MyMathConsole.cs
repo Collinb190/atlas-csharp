af93727725396f7fe56f670c1d02913665954bfd
Collinb190 <collinb190@gmail.com>
2024-05-11 12:06:55 -0500

    task 0 warmup

---
+++ csharp-tdd/0-add/MyMathConsole/MyMathConsole.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using MyMath;
+
+namespace MyMathConsole
+{    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 5;
+
+            int sum = Operations.Add(a, b);
+            Console.WriteLine($"The sum is {sum}");
+        }
+    }
+}
