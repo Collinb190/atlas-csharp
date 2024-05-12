40b30f34bd854918f9e08f09b1f4d31f7efb6dc7
Collinb190 <collinb190@gmail.com>
2024-05-12 10:19:22 -0500

    task 2

---
+++ csharp-tdd/2-max_int/MyMath/MyMath.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyMath
+{
+    /// <summary>
+    /// Class of math operations.
+    /// </summary>
+    public class Operations
+    {
+        /// <summary>
+        /// Finds the max integer in a list of integers
+        /// </summary>
+        /// <param name="nums">List to search</param>
+        /// <returns>Max integer in list nums</returns>
+        public static int Max(List<int> nums)
+        {
+            if (nums.Count < 1) return 0;
+
+            int result = int.MinValue;
+
+            foreach (int value in nums)
+            {
+                if (value > result)
+                {
+                    result = value;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ csharp-tdd/2-max_int/MyMathConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MyMath;
+using System.Collections.Generic;
+
+namespace MyMathConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> values = new List<int> { 6, 9, 12, 15, 18 };
+            int x = Operations.Max(values);
+            Console.WriteLine(x);
+        }
+    }
+}
