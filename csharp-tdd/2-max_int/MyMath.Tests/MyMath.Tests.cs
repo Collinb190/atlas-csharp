6e2df06c58e4a412af0c6b5b41554648703af43c
Collinb190 <collinb190@gmail.com>
2024-05-12 10:36:27 -0500

    task 2

--- csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
+++ csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
@@ -1,18 +1,41 @@
 using NUnit.Framework;
+using System.Collections.Generic;
 
-namespace Tests
+namespace MyMath.Tests
 {
-    public class Tests
+    [TestFixture]
+    public class OperatonsTests
     {
-        [SetUp]
-        public void Setup()
+        [Test]
+        public void ZeroValue()
         {
+            List<int> values = new List<int>();
+            int result = Operations.Max(values);
+            Assert.AreEqual(result, 0);
         }
 
         [Test]
-        public void Test1()
+        public void OneValue()
         {
-            Assert.Pass();
+            List<int> values = new List<int> { -3 };
+            int result = Operations.Max(values);
+            Assert.AreEqual(result, -3);
+        }
+
+        [Test]
+        public void AllPositive()
+        {
+            List<int> values = new List<int> { 45, 4, 78, 15, 22, 69, 420 };
+            int result = Operations.Max(values);
+            Assert.AreEqual(result, 420);
+        }
+
+        [Test]
+        public void AllNegative()
+        {
+            List<int> values = new List<int> { -45, -4, -78, -15, -22, -69, -420 };
+            int result = Operations.Max(values);
+            Assert.AreEqual(result, -4);
         }
     }
 }
