840b49e87139df0d99ff30a533cdd1b02122535d
Collinb190 <collinb190@gmail.com>
2024-05-12 09:45:14 -0500

    one files

---
+++ csharp-tdd/1-divide/MyMathConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyMathConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
