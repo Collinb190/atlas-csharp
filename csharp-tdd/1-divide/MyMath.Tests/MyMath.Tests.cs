141a1a16b1bd47bde35506d8b31ba2902f92f68b
Collinb190 <collinb190@gmail.com>
2024-05-13 00:01:39 -0500

    task 1

--- csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
+++ csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -6,13 +6,32 @@ namespace MyMath.Tests
     public class MatrixTests
     {
         [Test]
+        public void MatrixIsEmpty()
+        {
+            int[,] values = null; 
+            int num = 1;
+            int[,] result = Matrix.Divide(values, num);
+            Assert.AreEqual(result, null);
+        }
+
+        [Test]
+        public void NumIsZero()
+        {
+            int[,] values = new int[3, 2] { { 1, 2 }, { 3, 4 }, { 5, 6 } };
+            int[,] compare = new int[3, 2] {{ 0, 1 }, { 1, 2 }, { 2, 3 } }; 
+            int num = 0;
+            int[,] result = Matrix.Divide(values, num);
+            Assert.AreEqual(result, null);
+        }
+
+        [Test]
         public void Positive()
         {
             int[,] values = new int[3, 2] { { 1, 2 }, { 3, 4 }, { 5, 6 } };
             int[,] compare = new int[3, 2] {{ 0, 1 }, { 1, 2 }, { 2, 3 } }; 
             int num = 2;
             int[,] result = Matrix.Divide(values, num);
-            Assert.AreEqual(result,compare);
+            Assert.AreEqual(result, compare);
         }
     }
 }
