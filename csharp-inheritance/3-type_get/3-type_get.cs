d71f6bc4eb003fab358fd3cb9833088f922fa3a9
Collinb190 <collinb190@gmail.com>
2024-05-14 08:54:23 -0500

    task 3

---
+++ csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection;
+
+class Obj
+{
+    public static void Print(object myObj)
+    {
+        Type objType = myObj.GetType();
+
+        Console.WriteLine($"{objType.Name} Properties:");
+        foreach (PropertyInfo pInfo in objType.GetProperties())
+            Console.WriteLine(pInfo.Name);
+
+        Console.WriteLine($"{objType.Name} Methods:");
+        foreach (MethodInfo mInfo in objType.GetMethods())
+            Console.WriteLine(mInfo.Name);
+    }
+}
