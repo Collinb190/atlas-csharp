a7d6491ec87852943af380ecb70fbd4dfa6aad15
Collinb190 <collinb190@gmail.com>
2024-05-18 12:12:22 -0500

    task 6

---
+++ csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class VectorMath
+{
+    public static double[] Add(double[] vector1, double[] vector2)
+    {
+        double[] vectorSum = new double[vector1.Length];
+        double[] vectorSumFail = new double[1] { -1 };
+
+        if (vector1.Length != vector2.Length) 
+            return vectorSumFail;
+        if (vector1.Length != 2 && vector1.Length !=3)
+            return vectorSumFail;
+        for (int i = 0; i < vector1.Length; i++)
+        {
+            vectorSum[i] = vector1[i] + vector2[i];
+        }
+        return vectorSum;
+    }
+}
