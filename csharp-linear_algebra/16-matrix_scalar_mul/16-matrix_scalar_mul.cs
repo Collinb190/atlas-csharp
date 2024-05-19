87fc03230113d5db1bac06fbd80d0b8f2dc3b867
Collinb190 <collinb190@gmail.com>
2024-05-19 10:18:06 -0500

    task 16

---
+++ csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double[,] MultiplyScalar(double[,] matrix, double scalar)
+    {
+        int row1 = matrix.GetLength(0);
+        int col1 = matrix.GetLength(1);
+
+        double[,] fail = new double[,] { {-1} };
+        double[,] scaled = new double[row1, col1];
+
+        if (row1 != 2 && row1 != 3)
+            return fail;
+        for (int i = 0; i < row1; i++)
+            for (int j = 0; j < col1; j++)
+            {
+                scaled[i, j] = matrix[i, j] * scalar;
+            }
+        return scaled;
+    }
+}
