db11a21a4eb69501d6317eea9dab4d9e613aadd4
Collinb190 <collinb190@gmail.com>
2024-05-19 09:49:02 -0500

    task 14

---
+++ csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double[,] Add(double[,] matrix1, double[,] matrix2)
+    {
+        int row1 = matrix1.GetLength(0);
+        int col1 = matrix1.GetLength(1);
+        int row2 = matrix2.GetLength(0);
+        int col2 = matrix2.GetLength(1);
+        double[,] fail = new double[,] { {-1} };
+        double[,] sum = new double[row1, col1];
+
+        if (row1 != row2 || col1 != col2)
+            return fail;
+        if (row1 != 2 && row1 != 3)
+            return fail;
+        for (int i = 0; i < row1; i++)
+            for (int j = 0; i < col1; j++)
+            {
+                sum[i, j] = matrix1[i, j] + matrix2[i, j];
+            }
+        return sum;
+    }
+}
