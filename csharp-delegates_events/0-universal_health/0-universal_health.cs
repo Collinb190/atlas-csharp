dff6e198031ea5d66480dbc22aff9210508358c7
Collinb190 <collinb190@gmail.com>
2024-05-20 11:36:31 -0500

    task 0

---
+++ csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+class Player
+{
+    private string name;
+    private float maxHp;
+    private float hp;
+
+    /// <summary>
+    /// Player constructor
+    /// </summary>
+    /// <param name="name">Players name</param>
+    /// <param name="maxHp">total health</param>
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        this.name = name;
+        if (maxHp > 0) this.maxHp = maxHp;
+        else
+        {
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+            this.maxHp = 100f;
+        }
+        this.hp = this.maxHp;
+    }
+
+    /// <summary>
+    /// Prints the health of Player
+    /// </summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{this.name} has {this.hp} / {this.maxHp} health");
+    }
+}
