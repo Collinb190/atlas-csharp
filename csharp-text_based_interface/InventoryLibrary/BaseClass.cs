3795850f95152d9bea71fff5dd270efc6c45d651
Collinb190 <collinb190@gmail.com>
2024-05-21 11:10:50 -0500

    console project

---
+++ csharp-text_based_interface/InventoryLibrary/BaseClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace InventoryLibrary
+{
+    /// <summary> Class that all others will inherit from. </summary>
+    public class BaseClass
+    {
+        /// <summary> Unique id for the object. </summary>
+        public string id { get; set; }
+        /// <summary> Time the object was created </summary>
+        public DateTime date_created { get; set; }
+        /// <summary> Time the object was updated </summary>
+        public DateTime date_updated { get; set; }
+
+        /// <summary> Constructs a new instance of BaseClass. </summary>
+        public BaseClass()
+        {
+            id = Guid.NewGuid().ToString();
+            date_created = DateTime.Now;
+            date_updated = DateTime.Now;
+        }
+    }
+}
