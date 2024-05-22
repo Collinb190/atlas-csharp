eb26394acc871c43ff760ff63340dd84044e64e0
Collinb190 <collinb190@gmail.com>
2024-05-22 14:15:29 -0500

    las task

---
+++ csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class MatrixMath
+{
+    public static double[,] Inverse2D(double[,] matrix)
+    {
+        if (matrix.GetLength(0) != 2 || matrix.GetLength(1) !=2)
+            return new double[,] { {-1} };
+
+        double checkIfSingular = Determinant(matrix);
+        if (checkIfSingular == 0) return new double[,] { {-1} };
+
+        double a = matrix[0, 0];
+        double b = matrix[0, 1];
+        double c = matrix[1, 0];
+        double d = matrix[1, 1];
+
+        double[,] inverse = new double[,]
+        {
+            { d / det, -b / det },
+            { -c / det, a / det }
+        };
+
+        return inverse;
+    }
+
+    public static double Determinant(double[,] matrix)
+    {
+		if (matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2)
+			return Math.Round(((matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0])), 2);
+		else if (matrix.GetLength(0) == 3 && matrix.GetLength(1) == 3)
+		{
+			double a = matrix[0, 0] * ((matrix[1, 1] * matrix[2, 2]) - (matrix[1, 2] * matrix[2, 1]));
+			double b = matrix[0, 1] * ((matrix[1, 0] * matrix[2, 2]) - (matrix[1, 2] * matrix[2, 0]));
+			double c = matrix[0, 2] * ((matrix[1, 0] * matrix[2, 1]) - (matrix[1, 1] * matrix[2, 0]));
+			return Math.Round((a - b + c),2);
+		}
+		return - 1;
+    }
+}
