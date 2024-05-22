8ecc78c3f8959403b6c97bc9aacf955a81866e94
Collinb190 <collinb190@gmail.com>
2024-05-22 12:02:36 -0500

    task 23

--- csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
+++ csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -7,7 +7,7 @@ class MatrixMath
         double[,] result = new double[matrix.GetLength(1), matrix.GetLength(0)];
 
         for (int i = 0; i < matrix.GetLength(0); i++)
-            for (int j = 0; j < matrix.GetLength(1) i++)
+            for (int j = 0; j < matrix.GetLength(1); i++)
                 result[j, i] = matrix[i, j];
         return result;
     }
